391245c5c9db52973bc733cbb153f824b753ef3e
Dor Rubin <dor.rubin@gmail.com>
2015-06-23 18:51:27 -0400

    sign in validation

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,9 @@ Rails.application.routes.draw do
   root 'welcomes#index'
 
   get '/login' => 'sessions#new'
-
   post '/login' => 'sessions#create'
-
   delete '/logout' => 'sessions#destroy'
-
+  
   resources :faculties, :students
 
   resources :faculties do
