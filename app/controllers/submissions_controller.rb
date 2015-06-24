abfae1271ef6112268220674df4fc45ddb42923c
Dor Rubin <dor.rubin@gmail.com>
2015-06-24 16:58:44 -0400

    stuck on courses submissions new

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -3,14 +3,12 @@ class SubmissionsController < ApplicationController
   before_action :students_only
 
   def new
-    @submission = Submission.new
-    binding.pry
+    @student = Student.find(session[:user_id])
+    @submission = @student.submissions.new(course_id: params[:course_id]) unless session[:user_id].nil?
   end
 
   def create
-    @submissions = Submission.where(student_id: session[:user_id])
     @submission = Student.find(session[:user_id]).submissions.new(course_id: params[:course_id])
-    binding.pry
   end
 
   private
