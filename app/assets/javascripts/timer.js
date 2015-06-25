312d20e49e58806084d2166016cae8bce2d4e6f2
Max Peterson <petersonmaxr@gmail.com>
2015-06-25 11:14:26 -0400

    started adding the timer

---
+++ app/assets/javascripts/timer.js
@@ -0,0 +1,10 @@
+var timeDisplay = document.querySelector("#time-display");
+var start = document.querySelector("#start-timer");
+var stop = document.querySelector("#stop-timer");
+var reset = document.querySelector("#reset-timer");
+
+var time = 600;
+
+var decrementTime = function() {
+	
+};
