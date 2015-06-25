bbdb1b2bfa4034ba5895c8b859619a25d4084863
Anila Alexander <anila.j.alexander@gmail.com>
2015-06-25 12:01:00 -0400

    switching braches

--- app/controllers/officers/submissions_controller.rb
+++ app/controllers/officers/submissions_controller.rb
@@ -18,7 +18,7 @@ module Officers
 
 		def create
 			submission = Submission.find(params[:id])
-			submission.phonescreen = Phonescreen.new(params[:phonescreen].permit(:q1, :q2, :q3, :q4, :q5))
+			submission.phonescreen = Phonescreen.new(params[:phonescreen].permit(:q1, :q2, :q3, :q4, :q5, :notes))
 			if submission.phonescreen.save
 		redirect_to "/officers/submissions"
 
