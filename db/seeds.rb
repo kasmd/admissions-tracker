9e69d9ec13bc351848ef6b1525bd0e4d52d10e70
Dor Rubin <dor.rubin@gmail.com>
2015-06-25 10:38:56 -0400

    seeded db with phone and interview data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,8 @@ Instructor.destroy_all
 Officer.destroy_all
 Course.destroy_all
 Submission.destroy_all
+Phone.destroy_all
+Interview.destroy_all
 
 student_1 = Student.create({f_name: "Student", l_name: "Testberg", email: "s1@test.com", password: "jk", phone_number: "#{FFaker::PhoneNumber.phone_number}" })
 student_2 = Student.create({f_name: "Another", l_name: "Estudiante", email: "s2@test.com", password: "jk", phone_number: "#{FFaker::PhoneNumber.phone_number}" })
@@ -40,13 +42,21 @@ Course.create({subject: "UXI", start_date:"2015-07-01", end_date:"2015-11-30", l
 pmi = Course.create({subject: "PMI", start_date:"2015-06-23", end_date:"2015-7-31", location:"#{FFaker::Address.city}", price: 8000})
 Course.create({subject: "PMI", start_date:"2015-08-31", end_date:"2015-10-31", location:"#{FFaker::Address.city}", price: 8000})
 
-Submission.create(student: student_1, course: wdi, status: "accepted", instructor_id: instructor, officer_id: officer)
+pass_phone = Phone.create({q1: 5, q2: 4, q3: 4, q4: 5, q5: 5, notes: "This phone interview went great"})
+
+pass_interview = Interview.create({q1: 3, q2: 4, q3: 4, q4: 4, q5: 4, notes: "This in person interview was fantastic"})
+
+phone_fail = Phone.create({q1: 2, q2: 1, q3: 3, q4: 2, q5: 1, notes: "This person is not qualified. Phone interview was terrible. Doesn't know the difference between a rails migration and a swallow migration."})
+
+Submission.create(student: student_1, course: wdi, status: "accepted", instructor_id: instructor, officer_id: officer, phone_id: pass_phone.id, interview_id: pass_interview.id)
 
 Submission.create(student: student_1, course: uxi, status: "pending-phone")
 
 Submission.create(student: student_2, course: wdi, status: "pending-phone", officer_id: officer)
 
-Submission.create(student: student_2, course: pmi, status: "rejected", officer_id: officer)
+Submission.create(student: student_2, course: pmi, status: "rejected", officer_id: officer, phone_id: phone_fail.id)
+
+binding.pry
 
 # QUESTIONNAIRE
 
