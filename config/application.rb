80e0a190020758d99ccff265ac35c57ec59a2134
Anila Alexander <anila.j.alexander@gmail.com>
2015-06-28 10:25:26 -0400

    committing

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,7 @@ module AdmissionsTracker
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.paths << "#{Rails.root}/app/assets/videos"
+
   end
 end
