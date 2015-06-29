4e05a502cdd34e332bb6ca78ecc65db73c02e942
Max Peterson <petersonmaxr@gmail.com>
2015-06-29 09:39:51 -0400

    hard reset session[:redirect] on homepage load, i dunno, and then i added 'accept=.txt' to the submission upload

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,6 +1,9 @@
 class WelcomesController < ApplicationController
 
 	def index
+    if session[:redirect]
+    	session[:redirect] = nil
+    end
 		url = "http://api.randomuser.me/?results=3"
     response = RestClient.get(url)
     @randoms = JSON.parse(response).first[1]
